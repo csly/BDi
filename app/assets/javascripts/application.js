2cf9b144942373302a3e73627a93a56c581c3cc7
csly <carasly@me.com>
2015-10-26 14:50:13 +0000

    nomaterialjava

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,7 @@
 //
 //= require jquery   
 //= require jquery_ujs
-//= require bootstrap-sprockets 
-//= require materialize-sprockets
+//= require bootstrap-sprockets  
 //= require_tree .
 //= require ckeditor/init
 
