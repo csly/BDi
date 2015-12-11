927d809fefc1549411c6e5b632924f5348f6cfd2
csly <carasly@me.com>
2015-12-11 13:18:38 +0000

    twitter creds

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 $client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "SPD5aDFFWw82i7lMgw2gw"
-  config.consumer_secret     = "O10FL3ub1p2jdye8wvfJsPTCcpFtroyEDROgwU6AV8"
-  config.access_token        = "545107864-OgGEIpSw2umQsFfKquUw915JJxZ4MdWYziWA4aWH"
-  config.access_token_secret = "ITSjKT04j3Tgx0bmF7bSN3czPp6XREKIKoww13C6h9QnG"
+  config.consumer_key        = ENV['SPD5aDFFWw82i7lMgw2gw']
+  config.consumer_secret     = ENV['O10FL3ub1p2jdye8wvfJsPTCcpFtroyEDROgwU6AV8']
+  config.access_token        = ENV['545107864-OgGEIpSw2umQsFfKquUw915JJxZ4MdWYziWA4aWH']
+  config.access_token_secret = ENV['ITSjKT04j3Tgx0bmF7bSN3czPp6XREKIKoww13C6h9QnG']
 end
