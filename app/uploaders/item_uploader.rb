178ed29c93d5ec7b24eff9560b0330300ff9bba1
csly <carasly@me.com>
2016-10-14 13:24:36 +0100

    shop changes

--- app/uploaders/item_uploader.rb
+++ app/uploaders/item_uploader.rb
@@ -9,7 +9,7 @@ class ItemUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :file
+  storage :fog
  
 
   # Override the directory where uploaded files will be stored.
