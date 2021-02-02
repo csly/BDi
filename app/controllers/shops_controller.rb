48d0507ccb6768a01d088258795419f4ac703db6
csly <carasly@me.com>
2021-02-02 19:19:32 +0000

    shop changes

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,5 +1,20 @@
 class ShopsController < ApplicationController
   def index 
-    @shop_items = ShopItem.order('created_at DESC')
+    if params[:q]
+      @shop_items = ShopItem.search(params[:q]).paginate(page: params[:page], per_page: 25)
+       
+      if @shop_items.blank?
+        redirect_to shops_path(@shop_items), notice: 'There are no items that match your search requirements. Please try again'
+      end
+      else
+      params[:limit] ||= 25
+      @shop_items = ShopItem.ordered_by_artist.paginate(page: params[:page], per_page: params[:limit])
+       
+    end
   end
 end
+
+
+   
+
+   
--- app/models/shop_item.rb
+++ app/models/shop_item.rb
@@ -4,7 +4,21 @@ class ShopItem < ActiveRecord::Base
 
   delegate :artist, to: :artist_shop
 
+  scope :ordered_by_artist, -> {
+    joins(artist_shop: :artist)
+    .order('artists.name asc, LOWER(shop_items.name) asc')
+  }
+
   def preview
     description[0..100].strip + '...'
   end
+
+  class << self
+    def search(query)
+      query = (query && !query.empty?) ? "%#{query}%" : nil
+      return ShopItem.ordered_by_artist unless query
+      ShopItem.ordered_by_artist.where('shop_items.name ILIKE ? or shop_items.description ILIKE ?', query, query)
+    end
+  end
+
 end
