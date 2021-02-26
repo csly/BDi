bb218f9c7dcef32afd6e80e01b45ab757fd47446
csly <carasly@me.com>
2021-02-26 16:20:45 +0000

    use mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,12 +78,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'bdimusic.com',
+    :authentication => :plain,
   }
+
+  ActionMailer::Base.delivery_method = :smtp
 end
